8255937e324760d9eefa1f2c21a7fd415fa75a55
Eugene Karpenko <notformat@gmail.com>
2022-12-10 18:36:50 +0200

    Add Lindenmayer system

--- Turtle.py
+++ Turtle.py
@@ -22,6 +22,29 @@ pygame.display.set_caption("OpenGL in Python")
 
 current_position = (0, 0)
 direction = np.array([0, 1, 0])
+axiom = 'F'
+rules = {
+    "F": "F[+F]F"
+}
+draw_length = 10
+angle = 90
+stack = []
+rule_number = 5
+instructions = ""
+
+def run_rule(run_count):
+    global instructions
+    instructions = axiom
+    for loops in range (run_count):
+        old_system = instructions
+        instructions = ""
+        for c in range (0, len(old_system)):
+            if (old_system[c] in rules):
+                instructions += rules[old_system[c]]
+            else:
+                instructions += old_system[c]
+    print("Rile")
+    print(instructions)
 
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
@@ -67,6 +90,8 @@ init_ortho()
 
 glLineWidth(1.0)
 glPointSize(1.0)
+
+run_rule(rule_number)
 
 while not done:
     for event in pygame.event.get():
