68132485d7568e82e7391eac99696344e68d161a
Eugene Karpenko <notformat@gmail.com>
2022-12-17 13:21:48 +0200

    Add Light object

--- Engine2/Projections.py
+++ Engine2/Projections.py
@@ -7,6 +7,7 @@ from glapp.Triangle import *
 from glapp.Axes import *
 from glapp.Cube import *
 from glapp.LoadMesh import *
+from glapp.Light import *
 
 vertex_shader = r'''
 #version 330
@@ -20,7 +21,6 @@ out vec3 color;
 out vec3 normal;
 out vec2 uv;
 out vec3 frag_pos;
-out vec3 light_pos;
 out vec3 view_pos;
 
 uniform mat4 projection_mat;
@@ -29,7 +29,6 @@ uniform mat4 view_mat;
 
 void main()
 {
-    light_pos = vec3(2, 2, 5);
     view_pos = vec3(vec4(view_mat[3][0], view_mat[3][1], view_mat[3][2], 1));
     //light_pos = vec3(view_mat[3][0], view_mat[3][1], view_mat[3][2]);
     gl_Position = projection_mat * inverse(view_mat)  * model_mat * vec4(position, 1);
@@ -43,39 +42,42 @@ void main()
 fragment_shader = r'''
 #version 330 core
 
+#define NUM_LIGHTS 2
+
 in vec3 color;
 in vec3 normal;
 in vec2 uv;
 in vec3 frag_pos;
-in vec3 light_pos;
 in vec3 view_pos;
 
 out vec4 frag_color;
 
-void main()
+struct Light
 {
-    vec3 light_color = vec3(1, 0, 0);
+    vec3 position;
+    vec3 color;
+};
 
+uniform Light lights_data[NUM_LIGHTS];
+
+vec4 CreateLight(vec3 lightPos, vec3 lightColor, vec3 normal, vec3 fragPos, vec3 viewPos)
+{
     //ambient
     float a_strength = 0.1;
-    vec3 ambient = a_strength * light_color;
+    vec3 ambient = a_strength * lightColor;
 
     // diffuse
     float diff_strength = 0.3;
     vec3 norm = normalize(normal);
-    vec3 light_dir = normalize(light_pos - frag_pos);
+    vec3 light_dir = normalize(lightPos - fragPos);
     float dif = max(dot(norm, light_dir), 0.0);
-    vec3 diffuse = light_color * dif * diff_strength;
+    vec3 diffuse = lightColor * dif * diff_strength;
 
     // specular
     float s_strenth = 1.0;
-    vec3 view_dir = normalize(view_pos-frag_pos);
-    vec3 reflect_dir = normalize(-light_dir -norm);
-    float spec = pow(max(dot(view_dir, reflect_dir), 0), 128);
-    
 
-    vec3 direction = frag_pos - light_pos;
-    vec3 fragToEye = normalize(view_pos - frag_pos);
+    vec3 direction = fragPos - lightPos;
+    vec3 fragToEye = normalize(viewPos - fragPos);
     vec3 reflectedVertex = normalize(reflect(direction, normalize(normal)));
     float specularFactor = dot(fragToEye, reflectedVertex);
 
@@ -88,16 +90,31 @@ void main()
         specularFactor = 0;
     }
 
-    vec3 specular = s_strenth * specularFactor * light_color;
+    vec3 specular = s_strenth * specularFactor * lightColor;
 
-    vec3 ee = vec3(specularFactor, specularFactor, specularFactor);
+    return vec4(ambient + diffuse + specular, 1);
+}
 
+vec4 CalcAllPoints()
+{
+    int lightsCount = 2;
 
-    frag_color = vec4(color * (ambient + diffuse + specular), 1);
+    vec4 final_color = vec4(0, 0, 0, 1);
 
-    //frag_color = vec4(reflect_dir, 1);
+    for (int i = 0; i < lightsCount; i++)
+    {
+        final_color += CreateLight(lights_data[i].position, lights_data[i].color, normal, frag_pos, view_pos);
+    }
 
+    return final_color;
+}
 
+void main()
+{
+    vec4 final_color = vec4(color, 1); 
+    final_color *= CalcAllPoints();
+
+    frag_color = final_color;
 }
 '''
 
@@ -113,6 +130,9 @@ class Projections(PyOGLApp):
         self.crah = None
         self.cortex = None
 
+        self.light_0 = None
+        self.light_1 = None
+
     def initialize(self):
         self.program_id = create_program(vertex_shader, fragment_shader)
         #self.square = Square(self.program_id, pygame.Vector3(0, -1, -1))
@@ -127,6 +147,13 @@ class Projections(PyOGLApp):
         self.monkey = LoadMesh("Resources/monkey_hd.obj", self.program_id, location=pygame.Vector3(4, 1.5, 0))
         self.crash = LoadMesh("Resources/crash.obj", self.program_id, location=pygame.Vector3(-4, 0.0, 0))
         self.cortex = LoadMesh("Resources/donut.obj", self.program_id, location=pygame.Vector3(0, 4.5, 0))
+
+        #self.light_0 = Light(self.program_id, pygame.Vector3(5, 5, 5), pygame.Vector3(0, 0.341, 0.717), 0)
+        #self.light_1 = Light(self.program_id, pygame.Vector3(-5, 5, 5), pygame.Vector3(1.0, 0.843, 0), 1)
+
+        self.light_0 = Light(self.program_id, pygame.Vector3(-5, 5, 5), pygame.Vector3(0, 0.0, 1.0), 0)
+        self.light_1 = Light(self.program_id, pygame.Vector3(5, 5, 5), pygame.Vector3(1.0, 0.843, 0), 1)
+
         glEnable(GL_DEPTH_TEST)
 
     def camera_init(self):
@@ -136,6 +163,9 @@ class Projections(PyOGLApp):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glUseProgram(self.program_id)
         self.camera.update()
+
+        self.light_0.update()
+        self.light_1.update()
         
         self.teapot.update()
         self.monkey.update()
